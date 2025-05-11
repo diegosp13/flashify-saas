03043890a23218016d412554ca458c2b9fcc9eac
diegosp13 <146869699+diegosp13@users.noreply.github.com>
2025-05-11 05:29:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import InstagramToolsPage from './pages/InstagramToolsPage';
+import Editor from './pages/Editor';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/instagram" element={<InstagramToolsPage />} />
+        <Route path="/editor" element={<Editor />} />
       </Routes>
     </BrowserRouter>
   );
